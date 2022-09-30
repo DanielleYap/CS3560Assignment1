b37d03c1c62f56e0b9bc2704938d536afdcddbe3
Danielle Ya <dpyap@cpp.edu>
2022-09-29 22:59:53 -0700

    final?

--- src/SimulationDriver.java
+++ src/SimulationDriver.java
@@ -6,35 +6,31 @@ import java.util.UUID;
 public class SimulationDriver {
     public static void main(String[] args) {
         Random rand = new Random();
-        /* **********************************************************************************************************
-         *
-         * Simulating multiple choice question
-         *
-         * **********************************************************************************************************/
+        VotingService service = new VotingService();
+        final int TYPE = 2; //makes it easier to switch between modes, 1=multi, 2=single
 
         /* *********************************************************
          * 1) Create a question type and configure the answers
          * *********************************************************/
         // Create question text
-        String question1Text = "1. What is Object Oriented Programming?";
+        String questionText = "1. What is Object Oriented Programming?";
 
         // Create answers
-        HashMap<Integer, String> question1Answers = new HashMap<Integer, String>();
-        question1Answers.put(0, "\tA. Organized");
-        question1Answers.put(1, "\tB. Difficult");
-        question1Answers.put(2, "\tC. A way to design programs");
-        question1Answers.put(3, "\tD. Something we are learning");
-        question1Answers.put(4, "\tE. Helpful");
+        HashMap<Integer, String> questionAnswers = new HashMap<>();
+        questionAnswers.put(0, "\tA. Organized");
+        questionAnswers.put(1, "\tB. Difficult");
+        questionAnswers.put(2, "\tC. A way to design programs");
+        questionAnswers.put(3, "\tD. Something we are learning");
+        questionAnswers.put(4, "\tE. Helpful");
 
-        // Generate MCQ object, stores question text and answers
-        Question question1 = new Question(1, question1Text, question1Answers);
+        // Generate Question object, stores question text and answers
+        Question question = new Question(TYPE, questionText, questionAnswers);
 
         /* *********************************************************
          * 2) Configure the question for the VotingService
          * ********************************************************/
         // Send question to VotingService
-        VotingService service = new VotingService();
-        service.setQuestion(question1);
+        service.setQuestion(question);
 
         /* *********************************************************
          * 3) Randomly generate a number of students and their answers
@@ -42,42 +38,40 @@ public class SimulationDriver {
          * 5) Call the VotingService output function
          * *********************************************************/
         // Generate random number of students (15-30)
-        int studentCount = rand.nextInt(15)+15;
+        // int studentCount = rand.nextInt(15)+15;
+        int studentCount = 5;
 
         // Create an array of Students with unique IDs
         Student studentList[] = new Student[studentCount];
+
         for (int i=0; i<studentCount; i++) {
             // UUID creates unique IDs for each student
             studentList[i] = new Student (UUID.randomUUID());
         }
 
-
         // It's a mess, but it works
-        HashMap<String, HashSet<Integer>> submissions = new HashMap<String, HashSet<Integer>>();
         for (int i=0; i<studentCount; i++) {
+            HashSet<Integer> responses = new HashSet<>();
             // Randomly generate student's submissions
-            int answersChosen = rand.nextInt(question1.getAnswers().size()-1)+2;
-            // Add submission to Student's response HashSet
+            int answersChosen = rand.nextInt(question.getAnswers().size()-1)+2;
+            if (question.getType()==2) {answersChosen=2;}
+            // Add Student and their submission(s) to VotingService
             for (int j=1; j<answersChosen; j++) {
-                studentList[i].getResponse().add(rand.nextInt(question1.getAnswers().size()));
-            }
-            // Puts ID and submission together in a Hashmap
-            submissions.put(studentList[i].getStudentID().toString(), studentList[i].getResponse());
-        }
+                int chosen = question.getAnswers().size();
+                responses.add(rand.nextInt(chosen));
 
-        // Submit answers to VotingService
-        service.setResponseSubmissions(submissions);
+                service.acceptSubmissions(studentList[i], responses);
+            }
+
+            //studentList[i].getResponse().add(rand.nextInt(chosen));
+            //service.acceptSubmissions(studentList[i]);
+
+            // Mark Student as submitted
+            service.getSubmitted().add(studentList[i]);
+        }
 
         // Call output function
         service.outputSubmissionStats();
-
-
-        /* **********************************************************************************************************
-         *
-         * Testing single choice question
-         *
-         * **********************************************************************************************************/
-
 
     }
 }
